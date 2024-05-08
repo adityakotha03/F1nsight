0c13b86eed6878c5deec6c04a3cec45107a1c69b
adityakotha03 <kothaaditya03@gmail.com>
2024-05-08 07:03:32 +0530

    Updated BroswerRouter to HAshRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
