6dc711de41ca104b08b377423d0f58569f116949
AntDotComm <32787612+ant-dot-comm@users.noreply.github.com>
2024-05-12 08:28:08 -0700

    results ui update

--- src/components/ConstructorCar.js
+++ src/components/ConstructorCar.js
@@ -4,53 +4,59 @@ import classNames from 'classnames';
 import { useInView } from "framer-motion";
 
 export const ConstructorCar = (props) => {
-    const { className, points, image, name, year, drivers} = props;
+    const { className, points, image, name, year, drivers, index} = props;
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true });
+
 
     return (
         <div 
             className={classNames(
                 className, 
-                'constructor-card mt-64'
+                'constructor-card mt-32'
             )}
             ref={ref}
         >
             <div className="flex flex-col items-center">
-                <p className="heading-4 -mb-4 constructor-title gradient-text-dark">{name}</p>
-                <span className="heading-1">{points}</span>
-                <div className="flex items-end relative">
-                    <img 
-                        alt="" 
-                        className="-mt-40 -mr-80 sm:-mr-40"
-                        height={85} 
-                        src={`${process.env.PUBLIC_URL + "/images/" + year + "/drivers/" + drivers[0] + ".png"}`}
-                        width={120} 
-                        style={{
-                            opacity: isInView ? 1 : 0,
-                            transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) .3s"
-                        }}
-                    />
+                <div div className="flex items-end mb-4">
+                    <div className="h-1 w-32 border-b-[1px] border-solid border-neutral-500" />
+                    <div className="font-display text-24 leading-none -mb-4 mx-8 text-neutral-500">
+                        {index + 1}
+                    </div>
+                    <div className="h-1 w-32 border-b-[1px] border-solid border-neutral-500" />
+                </div>
+                <p className="uppercase tracking-sm text-xl">{name.replace('F1 Team', '')}</p>
+                <div className="h-1 w-[9.6rem] border-b-[1px] border-solid border-neutral-500 mb-4 mt-4" />
+                <span className="heading-1 gradient-text-light ">{points}</span>
+                <div className="flex items-end relative -mt-32">
                     {drivers[3] && (
                         <img 
                             alt="" 
-                            className="absolute left-[5rem]"
-                            height={85} 
+                            className="absolute left-[-4rem]"
                             src={`${process.env.PUBLIC_URL + "/images/" + year + "/drivers/" + drivers[3] + ".png"}`}
-                            width={120} 
+                            width={90} 
                             style={{
                                 opacity: isInView ? 1 : 0,
                                 transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) .4s"
                             }}
                         />
                     )}
+                    <img 
+                        alt="" 
+                        className="-mt-40 -mr-80 sm:-mr-60"
+                        src={`${process.env.PUBLIC_URL + "/images/" + year + "/drivers/" + drivers[0] + ".png"}`}
+                        width={100} 
+                        style={{
+                            opacity: isInView ? 1 : 0,
+                            transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) .3s"
+                        }}
+                    />
 
                     <img 
                         alt="" 
                         className="-mt-16 -mb-32 z-10"
-                        height={85} 
                         src={`${process.env.PUBLIC_URL + "/images/" + year + "/cars/" + image + ".png"}`}
-                        width={288} 
+                        width={264} 
                         style={{
                             transform: isInView ? "none" : "translateX(-300px)",
                             opacity: isInView ? 1 : 0,
@@ -60,10 +66,9 @@ export const ConstructorCar = (props) => {
 
                     <img 
                         alt="" 
-                        className="-mt-40 -ml-80 sm:-ml-40"
-                        height={85} 
+                        className="-mt-40 -ml-80 sm:-ml-60"
                         src={`${process.env.PUBLIC_URL + "/images/" + year + "/drivers/" + drivers[1] + ".png"}`}
-                        width={120} 
+                        width={100} 
                         style={{
                             opacity: isInView ? 1 : 0,
                             transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) .3s"
@@ -72,10 +77,9 @@ export const ConstructorCar = (props) => {
                     {drivers[2] && (
                         <img 
                             alt="" 
-                            className="absolute right-[5rem]"
-                            height={85} 
+                            className="absolute right-[-4rem]"
                             src={`${process.env.PUBLIC_URL + "/images/" + year + "/drivers/" + drivers[2] + ".png"}`}
-                            width={120} 
+                            width={90} 
                             style={{
                                 opacity: isInView ? 1 : 0,
                                 transition: "all 2s cubic-bezier(0.17, 0.55, 0.55, 1) .4s"
