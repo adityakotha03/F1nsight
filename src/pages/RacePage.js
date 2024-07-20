748797a36bfb2f3ca0de7e3d308e870627443ad6
AntDotComm <32787612+ant-dot-comm@users.noreply.github.com>
2024-07-19 23:13:12 -0700

    hungary map

--- src/pages/RacePage.js
+++ src/pages/RacePage.js
@@ -82,7 +82,7 @@ export function RacePage() {
     };
 
 
-    const animatedLocations = ["Sakhir", "Suzuka", "Melbourne", "Monaco", "Silverstone"];
+    const animatedLocations = ["Sakhir", "Suzuka", "Melbourne", "Monaco", "Silverstone", "Budapest"];
 
     const selectedDriverData = drivers.find(
         (obj) => obj["acronym"] === driverCode
