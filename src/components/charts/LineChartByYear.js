1d87a29ea45999d944c9f897fefd6099342c85b8
adityakotha03 <kothaaditya03@gmail.com>
2024-09-23 20:39:52 +0530

    prefetch all races

--- src/components/charts/LineChartByYear.js
+++ src/components/charts/LineChartByYear.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-export const LineChartByYear = ({ driver1Name, driver2Name, driver1Data, driver2Data, competingYears, displayCompetingYears, type }) => {
+export const LineChartByYear = ({ driver1Name, driver2Name, driver1Data, driver2Data, competingYears, displayCompetingYears, type, raceNamesByYear }) => {
   const [selectedYear, setSelectedYear] = useState(competingYears[0]);
 
   useEffect(() => {
@@ -18,26 +18,38 @@ export const LineChartByYear = ({ driver1Name, driver2Name, driver1Data, driver2
   const getPositionData = (year) => {
     const driver1Positions = driver1Data[year]?.positions || {};
     const driver2Positions = driver2Data[year]?.positions || {};
+    
+    const races = raceNamesByYear[year] && raceNamesByYear[year].length > 0 ? raceNamesByYear[year] : Object.keys(driver1Positions); // Use race names if available
 
-    const races = Array.from(new Set([...Object.keys(driver1Positions), ...Object.keys(driver2Positions)]));
-
-    return races.map(race => ({
+    const data = races.map(race => ({
       race,
-      [driver1Name]: parseInt(driver1Positions[race] || 0, 10),
-      [driver2Name]: parseInt(driver2Positions[race] || 0, 10),
+      [driver1Name]: driver1Positions[race] ? parseInt(driver1Positions[race], 10) : undefined,
+      [driver2Name]: driver2Positions[race] ? parseInt(driver2Positions[race], 10) : undefined,
+    })).filter(data => data[driver1Name] !== undefined || data[driver2Name] !== undefined); // Exclude entries with no data
+
+    return data.map(data => ({
+      race: data.race,
+      [driver1Name]: data[driver1Name] !== undefined ? data[driver1Name] : 'DNP', // Default to Did not pariticpate (DNP) if not found
+      [driver2Name]: data[driver2Name] !== undefined ? data[driver2Name] : 'DNP', 
     }));
   };
 
   const getCumulativePointsData = (year) => {
     const driver1Points = driver1Data[year]?.pos || {};
     const driver2Points = driver2Data[year]?.pos || {};
+    
+    const races = raceNamesByYear[year] && raceNamesByYear[year].length > 0 ? raceNamesByYear[year] : Object.keys(driver1Points); // Use race names if available
 
-    const races = Array.from(new Set([...Object.keys(driver1Points), ...Object.keys(driver2Points)]));
-
-    return races.map((race, index) => ({
+    const data = races.map(race => ({
       race,
-      [driver1Name]: driver1Points[race]?.points || 0,
-      [driver2Name]: driver2Points[race]?.points || 0,
+      [driver1Name]: driver1Points[race]?.points || undefined,
+      [driver2Name]: driver2Points[race]?.points || undefined,
+    })).filter(data => data[driver1Name] !== undefined || data[driver2Name] !== undefined); // Exclude entries with no data
+
+    return data.map(data => ({
+      race: data.race,
+      [driver1Name]: data[driver1Name] !== undefined ? data[driver1Name] : 'DNP', 
+      [driver2Name]: data[driver2Name] !== undefined ? data[driver2Name] : 'DNP', 
     }));
   };
 
@@ -75,10 +87,15 @@ export const LineChartByYear = ({ driver1Name, driver2Name, driver1Data, driver2
         <LineChart data={type === 'position' ? positionData : cumulativePointsData} margin={{ top: 20, right: 30 }}>
           <CartesianGrid strokeDasharray="3 3" stroke="#444444" />
           <XAxis dataKey="race" tick={<CustomizedXAxisTick />} />
-          <YAxis reversed={type === 'position' ? true : false} domain={[1, 'dataMax']} />
+          <YAxis reversed={type === 'position'} domain={[1, 'dataMax']} />
           <Tooltip 
             labelFormatter={(name) => name} 
-            formatter={(value) => type === 'position' ? `P${value}` : `${value} points`}
+            formatter={(value) => {
+              if (value === 'DNP') {
+                return 'DNP';
+              }
+              return type === 'position' ? `P${value}` : `${value} points`;
+            }}
           />
           <Legend verticalAlign="top" height={32} />
           <Line type="monotone" dataKey={driver1Name} stroke="#8884d8" connectNulls={true}/>
