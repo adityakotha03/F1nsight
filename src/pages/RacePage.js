581c9e8de44cb262f18970c67b2795890dec226c
adityakotha03 <86183042+adityakotha03@users.noreply.github.com>
2024-11-03 17:06:40 +0530

    Update RacePage.js

--- src/pages/RacePage.js
+++ src/pages/RacePage.js
@@ -86,7 +86,7 @@ export function RacePage() {
     };
 
 
-    const animatedLocations = ["Sakhir", "Suzuka", "Melbourne", "Monaco", "Silverstone", "Budapest", "Spa-Francorchamps", "Zandvoort", "Monza", "Baku", "Marina Bay", "Austin", "Mexico City"];
+    const animatedLocations = ["Sakhir", "Suzuka", "Melbourne", "Monaco", "Silverstone", "Budapest", "Spa-Francorchamps", "Zandvoort", "Monza", "Baku", "Marina Bay", "Austin", "Mexico City", "São Paulo"];
 
     const selectedDriverData = drivers.find(
         (obj) => obj["acronym"] === driverCode
